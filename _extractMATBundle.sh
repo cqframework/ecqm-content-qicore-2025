9ad89f4a0effd836219180cee1427fe17f0a4fd8
Chris O Riordan <chris.oriordan@smilecdr.com>
2025-05-05 10:25:18 -0700

    CMS56 Export Refresh

--- _extractMATBundle.sh
+++ _extractMATBundle.sh
@@ -2,7 +2,7 @@
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-cli-3.7.0.jar
 input_cache_path=./input-cache
-mat_bundle=./bundles/mat/CMS50/CMS50FHIR-R1-MeasureExport/CMS50FHIR-v0.4.000-FHIR.json
+mat_bundle=./bundles/mat/CMS56/CMS56FHIR-R1-MeasureExport/CMS56FHIR-v1.1.000-FHIR.json
 
 set -e
 
