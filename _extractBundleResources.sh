46143b760a1e7af30f128e2a48a92d1135b441ae
Chris O Riordan <chris.oriordan@smilecdr.com>
2025-05-06 10:58:30 -0700

    CMS149 Export Refresh

--- _extractBundleResources.sh
+++ _extractBundleResources.sh
@@ -2,7 +2,7 @@
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-cli-3.7.0.jar
 input_cache_path=./input-cache
-mat_bundle=input/tests/measure/CMS146FHIRAppropriateTestingforPharyngitis
+mat_bundle=input/tests/measure/CMS149FHIRDementiaCognitiveAssessment
 
 
 set -e
